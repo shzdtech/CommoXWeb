eaedce6e6cc99325984e1b9973094971048f1335
Yile Liu <yile.liu@autodesk.com>
2016-07-18 09:37:59 +0800

    add multiple selection

--- src/Micro.Future.Commo.Web/Client/Scripts/Components/Filter.jsx
+++ src/Micro.Future.Commo.Web/Client/Scripts/Components/Filter.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 
 const Filter = React.createClass({
   render: function(){
-    var items =  this.props.items.map(function(item) {
+    let items =  this.props.items.map(function(item) {
                 return <li key={item.id}>
                     <a>
                         <span>{item.name}</span>
                     </a>
-                </li>
-            }); 
-     return <li>
+                </li>;
+            });
+    let rightSelector = null,
+        operators = null;
+    if(this.props.multipleSelection){
+        rightSelector = <div className='right-selector'>
+            <span className="multiply">多选</span>
+            <span className="togglebtn more">更多</span>
+            <span className="togglebtn retract">收起</span>
+        </div>;
+        operators = <div className='operator-btn'>
+            <span className="multiply-submit">提交</span>
+            <span className="calloff">取消</span>
+        </div>;
+    }
+
+     return <li className='filter-category'>
         <span className='title'>{this.props.title + ':'}</span>
         <ul className='items'>
            {items}
         </ul>
+        {rightSelector}
+        {operators}
     </li>;
   }
 });
