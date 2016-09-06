e57578223a91ef52eb0e77cf952170a206cca87e
Yile Liu <yile.liu@autodesk.com>
2016-09-06 09:24:55 +0800

    pre validate

--- src/Micro.Future.Commo.Web/Client/Scripts/Components/Account/ChangePassword.jsx
+++ src/Micro.Future.Commo.Web/Client/Scripts/Components/Account/ChangePassword.jsx
@@ -10,19 +10,28 @@ class ChangePassword extends React.Component {
 
     render() {
         let error = null;
-        if(this.props.password.newPassword !== this.props.password.newPasswordConfirm){
+        const {password} = this.props;
+        if (password.newPassword !== password.newPasswordConfirm) {
             error = <div className="error">新密码不一致</div>
         }
 
+        let disabled = false;
+        if (!password.newPassword || !password.newPasswordConfirm || !password.password || password.newPassword === ''
+            || password.newPasswordConfirm === '' || password.password === '') {
+            disabled = true;
+        }
+
         let inputs = this.props.forms.map((l) => {
-            return  <Input key={l.key} info={l} onChangeForm={this.props.onChangeForm} />
+            return <Input key={l.key} info={l} onChangeForm={this.props.onChangeForm} />
         })
         return <div className='change-password-container'>
             <div className="instruction">密码长度不少于8位，并且包含数字，大小字母写或符号</div>
             {inputs}
             {error}
             <div className='change-password-operators'>
-                <span className='btn btn-large submit' onClick={()=>{this.props.submitChangePassword(this.props.password)}}>提交</span>
+                <span className={'btn btn-large submit ' + (disabled || error !== null ? 'disabled' : '') } onClick={() => {
+                    error === null && !disabled && this.props.submitChangePassword(password)
+                } }>提交</span>
             </div>
         </div>
     }
--- src/Micro.Future.Commo.Web/Client/Scripts/Components/Account/CreateUser.jsx
+++ src/Micro.Future.Commo.Web/Client/Scripts/Components/Account/CreateUser.jsx
@@ -14,14 +14,22 @@ class CreateUser extends React.Component {
             type: 'text',
             key: 'email',
             length: 'medium',
-            placehodler: '邮箱'
+            placehodler: '请输入您的邮箱'
         };
+        let isValid = this.validateEmail(this.props.newUser.email);
+
         return <div className='create-user-container'>
             <Input info={u} onChangeForm={this.props.onChangeForm} />
             <div className='create-user-operators'>
-                <span className='btn btn-large submit' onClick={()=>{this.props.submitCreateUser(this.props.newUser)}}>提交</span>
+                <span className={'btn btn-large submit ' + (isValid ? '' : 'disabled') }
+                    onClick={() => { isValid && this.props.submitCreateUser(this.props.newUser) } }>提交</span>
             </div>
         </div>
+    }
+
+    validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
     }
 }
 
