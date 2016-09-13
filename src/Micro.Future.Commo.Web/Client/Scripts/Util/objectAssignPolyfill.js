9fd6eaf52735e6809d0f8e20dd08019037e64033
Yile Liu <yile.liu@autodesk.com>
2016-09-13 23:38:16 +0800

    fix eslint error

--- src/Micro.Future.Commo.Web/Client/Scripts/Util/objectAssignPolyfill.js
+++ src/Micro.Future.Commo.Web/Client/Scripts/Util/objectAssignPolyfill.js
@@ -1,15 +1,15 @@
 export default () => {
-  if (typeof Object.assign != 'function') {
+  if (typeof Object.assign !== 'function') {
     Object.assign = function (target) {
       'use strict';
-      if (target == null) {
+      if (target === null) {
         throw new TypeError('Cannot convert undefined or null to object');
       }
 
       target = Object(target);
       for (var index = 1; index < arguments.length; index++) {
         var source = arguments[index];
-        if (source != null) {
+        if (source !== null) {
           for (var key in source) {
             if (Object.prototype.hasOwnProperty.call(source, key)) {
               target[key] = source[key];
@@ -20,4 +20,4 @@ export default () => {
       return target;
     };
   }
-}
+};
