d24878c9f3d9bad890ccc7afe94decedef1a62d7
Yile Liu <yile.liu@autodesk.com>
2016-09-16 23:00:05 +0800

    fix bug about send mail button

--- src/Micro.Future.Commo.Web/Client/Scripts/Components/Account/EmailVerfication.jsx
+++ src/Micro.Future.Commo.Web/Client/Scripts/Components/Account/EmailVerfication.jsx
@@ -6,19 +6,21 @@ class EmailVerfication extends React.Component {
     constructor() {
         super();
         this.tick = this.tick.bind(this);
+        this.isTicking = false;
         this.onClickGetVerficationCode = this.onClickGetVerficationCode.bind(this);
     }
 
     render() {
         let isValidEmail = validateEmail(this.props.info.value);
+        let enabledSend = isValidEmail && !this.isTicking;
         let text = '获取验证码'
         return <Input {...this.props}>
-            <span className={'btn ' + (!isValidEmail ? 'disabled' : '') } onClick={isValidEmail && this.onClickGetVerficationCode} ref='getVerficationCode'>{text}</span>
+            <span className={'btn ' + (!enabledSend ? 'disabled' : '') } onClick={isValidEmail && this.onClickGetVerficationCode} ref='getVerficationCode'>{text}</span>
         </Input>
     }
 
     onClickGetVerficationCode() {
-        if (!$(this.refs.getVerficationCode).hasClass('disabled')) {
+        if (!$(this.refs.getVerficationCode).hasClass('disabled') && !this.isTicking) {
             this.tick();
             this.props.getVerficationCode(this.props.info.value);
         }
@@ -27,10 +29,12 @@ class EmailVerfication extends React.Component {
     tick() {
         var btn = this.refs.getVerficationCode;
         let tickCount = 120;
+        this.isTicking = true;
         $(btn).addClass('disabled');
         let ticker = setInterval(() => {
             $(btn).text('获取验证码(' + tickCount + ')')
             if (tickCount === 0) {
+                this.isTicking = false;
                 clearInterval(ticker);
                 $(btn).text('获取验证码');
                 $(btn).removeClass('disabled');
