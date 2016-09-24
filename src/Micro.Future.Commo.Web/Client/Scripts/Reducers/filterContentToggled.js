a70df8383efd030fb8b22cc45f5f4bbd2b0629f3
cqcmdwym <cqcmdwym@126.com>
2016-09-24 16:46:26 +0800

    show filter by default

--- src/Micro.Future.Commo.Web/Client/Scripts/Reducers/filterContentToggled.js
+++ src/Micro.Future.Commo.Web/Client/Scripts/Reducers/filterContentToggled.js
@@ -1,6 +1,6 @@
 import {TOGGLE_FILTER_LIST} from '../Constants/ActionTypes';
 
-const filterContentToggled = (state = { isIn: false }, action) => {
+const filterContentToggled = (state = { isIn: true }, action) => {
     switch (action.type) {
         case TOGGLE_FILTER_LIST:
             return {
