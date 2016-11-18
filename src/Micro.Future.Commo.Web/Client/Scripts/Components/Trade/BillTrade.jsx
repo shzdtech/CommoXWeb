b18e086d616f0ae2be4cfb970babd73e460c0e82
Yile Liu <neal_liu@outlook.com>
2016-11-18 05:45:21 -0800

    fund trade

--- src/Micro.Future.Commo.Web/Client/Scripts/Components/Trade/BillTrade.jsx
+++ src/Micro.Future.Commo.Web/Client/Scripts/Components/Trade/BillTrade.jsx
@@ -15,21 +15,21 @@ class BillTrade extends React.Component {
                     <p>小额承兑汇票<span className='quatation'>快速变现</span></p>
                     <p>票面<span className='quatation'>金额不限</span></p>
                     <p>不受承兑行限制</p>
-                </div> 
+                </div>
             </div>
             <div className="billstep">
-                    <ul>
-                        <li className="li1">
-                            <img src={book} />
-                        </li>
-                        <li className="li2">
-                            <img src={arrow} />
-                        </li>
-                        <li className="li3">
-                            <img src={billtrade} />
-                        </li>
-                    </ul>
-                </div>
+                <ul>
+                    <li className="li1">
+                        <img src={book} />
+                    </li>
+                    <li className="li2">
+                        <img src={arrow} />
+                    </li>
+                    <li className="li3">
+                        <img src={billtrade} />
+                    </li>
+                </ul>
+            </div>
         </div>
     }
 }
--- src/Micro.Future.Commo.Web/Client/Scripts/Components/Trade/FundTrade.jsx
+++ src/Micro.Future.Commo.Web/Client/Scripts/Components/Trade/FundTrade.jsx
@@ -5,9 +5,33 @@ class FundTrade extends React.Component {
         super();
     }
 
-    render() {     
+    render() {  
+        
+        let book = require('../../../Content/images/book.png');
+        let fundtrade = require('../../../Content/images/fundtrade.png');
+        let arrow = require('../../../Content/images/arrow.png');
+
         return <div>
-           
+            <div className='jumbotron'>
+                <div className='container'>
+                    <p>企业闲置资金</p>
+                    <p><span className='quatation strong'>活期理财</span></p>
+                    <p className='description'>80%的企业闲置资金在银行睡大觉的时候我们已经赚取了十几倍的利息</p>
+                </div>
+            </div>
+            <div className="billstep">
+                <ul>
+                    <li className="li1">
+                        <img src={book} />
+                    </li>
+                    <li className="li2">
+                        <img src={arrow} />
+                    </li>
+                    <li className="li3">
+                        <img src={fundtrade} />
+                    </li>
+                </ul>
+            </div>
         </div>
     }
 }
